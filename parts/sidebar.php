a22564324dfc7266d4cb1139dd7101589740c192
Jeffrey Blinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2026-03-21 22:23:09 +0100

    Put version in sidebar

--- parts/sidebar.php
+++ parts/sidebar.php
@@ -13,13 +13,23 @@
     <section>
         <nav>
             <a href="/">Home</a>
-            <a href="">About me</a>
+            <a href="/about/">About me</a>
             <a href="/posts/">Posts</a>
             <a href="/gear/">Gear</a>
-            <a href="">Places visited</a>
+            <a href="/visited/">Places visited</a>
             <?php
             // <a href="">Timeline</a>
             ?>
         </nav>
+    </section>
+    <section class="footer">
+        v3.0.1 | <?php
+            if (file_exists($_SERVER['DOCUMENT_ROOT']."/.deploy-revision")) {
+                $hash = file_get_contents($_SERVER['DOCUMENT_ROOT']."/.deploy-revision");
+                echo substr($hash, 0, 7);
+            } else {
+                echo "No commit hash (running local?)";
+            }
+        ?>
     </section>
 </aside>
