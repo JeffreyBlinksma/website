164f344e3c26304bbed068d9b2b5525e55b35983
Jeffrey Blinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2026-03-22 16:59:49 +0100

    Update CSS and certain pages

--- about/index.php
+++ about/index.php
@@ -4,30 +4,123 @@
     <?php
     require($_SERVER['DOCUMENT_ROOT']."/parts/meta.php");
     ?>
-    <title>Jeffrey Blinksma</title>
+    <title>About me | Jeffrey Blinksma</title>
     <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
 <?php
-require("parts/sidebar.php");
+require($_SERVER['DOCUMENT_ROOT']."/parts/sidebar.php");
 ?>
 <main>
     <h1>About me</h1>
     <p>Hi! I'm Jeff. I'm a sysadmin by day, and sound asleep by night. During my spare time I like to play around with home automation, networking gear and 3D printers. I also know how to code, which helps me create small quality of life automations.</p>
-    <h2>Contact</h2>
-    <table>
-        <tr>
-            <th>Email</th>
-            <td><a href="mailto:jeffweb@blinksma.eu">jeffweb@blinksma.eu</a></td>
-        </tr>
-        <tr>
-            <th>Mastodon</th>
-            <td><a href="https://glauca.space/@jeffreyblinksma">@jeffreyblinksma@glauca.space</a></td>
-        </tr>
-        <tr>
-            <th>Bluesky</th>
-            <td><a href="https://bsky.app/profile/jeffrey.blinksma.eu">@jeffrey.blinksma.eu</a></td>
-        </tr>
-    </table>
+    <section>
+        <h2 class="ul-blue">Contact</h2>
+        <table>
+            <tr>
+                <th>Email</th>
+                <td><a href="mailto:jeffweb@blinksma.eu">jeffweb@blinksma.eu</a></td>
+            </tr>
+            <tr>
+                <th>Mastodon</th>
+                <td><a href="https://glauca.space/@jeffreyblinksma">@jeffreyblinksma@glauca.space</a></td>
+            </tr>
+            <tr>
+                <th>Bluesky</th>
+                <td><a href="https://bsky.app/profile/jeffrey.blinksma.eu">@jeffrey.blinksma.eu</a></td>
+            </tr>
+            <tr>
+                <th>Signal</th>
+                <td><a href="https://signal.me/#eu/NBuY00gvl-PBqDU2dfBJphNi3bVvk-z4It3zhVqZOo7uNjtt_K9K87Ltu8CsyQ3g">@jblinksma.09</a>
+                </td>
+            </tr>
+            <tr>
+                <th>LinkedIn</th>
+                <td><a href="https://www.linkedin.com/in/jeffreyblinksma/">linkedin.com/in/jeffreyblinksma</a></td>
+            </tr>
+            <tr>
+                <th>GitHub</th>
+                <td><a href="https://github.com/JeffreyBlinksma">github.com/JeffreyBlinksma</a></td>
+            </tr>
+            <tr>
+                <th>ListenBrainz</th>
+                <td><a href="https://listenbrainz.org/user/jblinksma/">listenbrainz.org/user/jblinksma</a></td>
+            </tr>
+        </table>
+    </section>
+    <section>
+        <h2 class="ul-green">Jobs and Experience</h2>
+        <div class="work-entry">
+            <h3>
+                ICT Consultant
+                <a href="https://www.digizone.nl/">
+                    Digizone ICT B.V.
+                </a>
+                <span class="timespan">
+                    (2025/02 - present)
+                </span>
+            </h3>
+            <p>
+                ICT Consultant at Digizone ICT B.V., specializing in Microsoft 365, Intune, Exchange, 3CX, PaperCut Hive and the likes. Digizone ICT B.V. is an IT company that advises, supplies, manages, and maintains digital infrastructure, workplace hardware, cloud solutions, cybersecurity, and business telephony for small to medium-enterprises.
+            </p>
+        </div>
+        <div class="work-entry">
+            <h3>
+                Intern ICT Servicedesk
+                <a href="https://www.iperen.com/">
+                    Van Iperen B.V.
+                </a>
+                <span class="timespan">
+                    (2024/01 - 2024/07)
+                </span>
+            </h3>
+            <p>
+                Intern at the ICT Servicedesk of Van Iperen B.V., researching the practicality of implementing Mobile Device Management using Microsoft Intune in an organisation where the personal use of company issued devices is explicitly allowed. Van Iperen B.V. is a family-owned growth specialist that advises and supports farmers with sustainable solutions in nutrition, crop protection, biostimulants, soil and water quality to ensure healthy, resilient, and profitable crops.
+            </p>
+        </div>
+    </section>
+    <section>
+        <h2 class="ul-yellow">Volunteering</h2>
+        <div class="work-entry">
+            <h3>
+                Polling station volunteer
+                <a href="https://www.gemeentehw.nl/">
+                    Gemeente Hoeksche Waard
+                </a>
+                <span class="timespan">
+                    (2026/03)
+                </span>
+            </h3>
+            <p>blep blep blep</p>
+        </div>
+        <div class="work-entry">
+            <h3>
+                Webmaster
+                <a href="https://www.striengraaggezien.nl/">
+                    Stichting Strien ....... Graag gezien !!!
+                </a>
+                <span class="timespan">
+                    (2023/01 - present)
+                </span>
+            </h3>
+            <p>
+                Webmaster and postmaster for the Stichting Strien ....... Graag gezien !!!, a foundation aimed at promoting livability, safety, spatial quality, and public interest in Strijen's shopping area, while enhancing its economic vitality, regional appeal, and overall experience.
+            </p>
+        </div>
+        <div class="work-entry">
+            <h3>
+                Student Council
+                <a href="https://www.davinci.nl/">
+                    Da Vinci College
+                </a>
+                <span class="timespan">
+                    (2023/05 - 2024/07)
+                </span>
+            </h3>
+            <p>
+                During the academic years of 2022-2023 and 2023-2024 I held multiple positions in the student council of the Da Vinci College in Dordrecht, including being a student representative, but also executive functions like treasurer and ad interim vice-chairman. During this time we worked on multiple key issues, like the professionalization of the council, improving regional representation, monitoring institutional finances and increasing our standing in the national association of student councils.
+            </p>
+        </div>
+    </section>
 </body>
 </html>
