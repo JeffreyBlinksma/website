9ae3fd1f931099b5a38577c6be81e5fa22a87a10
JeffreyBlinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2026-04-06 22:23:22 +0200

    Upload v2 for timeline

---
+++ timeline/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <?php
+            require($_SERVER['DOCUMENT_ROOT']."/parts/meta.php");
+        ?>
+        <title>Timeline | Jeffrey Blinksma</title>
+        <link rel="stylesheet" href="/assets/style.css">
+    </head>
+    <body>
+        <?php
+            require($_SERVER['DOCUMENT_ROOT']."/parts/sidebar.php");
+        ?>
+        <main>
+            <h1>Timeline</h1>
+            <p class="subtitle"><i>Past versions of this website</i></p>
+        </main>
+        <footer>
+            <?php
+                require($_SERVER['DOCUMENT_ROOT']."/parts/footer.php");
+            ?>
+        </footer>
+    </body>
+</html>
