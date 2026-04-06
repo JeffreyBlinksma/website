8ff9711279cb85bd8e792f274f051b6a64fd72fb
Jeffrey Blinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2026-04-06 19:04:45 +0200

    OSM copyright should use HTTPS

--- visited/index.php
+++ visited/index.php
@@ -21,7 +21,7 @@
                 let map = L.map('map').setView([51.505, -0.09], 13);
                 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                     maxZoom: 19,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 }).addTo(map);
                 map.attributionControl.addAttribution('<a href="https://github.com/gorango/glyphs">gorango/glyphs</a>')
 
