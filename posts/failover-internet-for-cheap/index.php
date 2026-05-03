9e790038e915d2fb6eff916f15339d0777b881ee
Jeffrey Blinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2026-05-03 14:49:59 +0200

    Remove Matt Mullenweg from inspiration

---
+++ posts/failover-internet-for-cheap/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <?php
+        require($_SERVER['DOCUMENT_ROOT']."/parts/meta.php");
+        ?>
+        <meta name="title" property="og:title" content="Template post">
+        <meta name="description" property="og:description" content="Computer-breaking ICT consultant and student based in the Netherlands.">
+        <meta property="og:type" content="article">
+        <meta name="date" property="article:published_time" content="1970-01-01">
+        <title>Template post | Jeffrey Blinksma</title>
+        <link rel="stylesheet" href="/assets/style.css">
+    </head>
+    <body>
+        <?php
+            require($_SERVER['DOCUMENT_ROOT']."/parts/sidebar.php");
+        ?>
+        <main>
+            <h1>Test post</h1>
+            <p class="subtitle"><i>1 January 1970</i></p>
+            <a href="/posts">← back to posts</a>
+            <p>words words words content content content</p>
+        </main>
+        <footer>
+            <?php
+            require($_SERVER['DOCUMENT_ROOT']."/parts/footer.php");
+            ?>
+        </footer>
+    </body>
+</html>
