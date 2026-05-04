3a1f57e46c4c56c902b5a4eacac6160f6f525651
Jeffrey Blinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2026-05-04 21:05:57 +0200

    Split comments off to seperate file

--- posts/.template/index.php
+++ posts/.template/index.php
@@ -20,25 +20,9 @@
             <p class="subtitle"><i>1 January 1970</i></p>
             <a href="/posts">← back to posts</a>
             <p>words words words content content content</p>
-            <section>
-                <h2>Comments</h2>
-                <script src="https://giscus.app/client.js"
-                        data-repo="JeffreyBlinksma/website"
-                        data-repo-id="R_kgDORbwP2Q"
-                        data-category="Comments"
-                        data-category-id="DIC_kwDORbwP2c4C8RbU"
-                        data-mapping="og:title"
-                        data-strict="0"
-                        data-reactions-enabled="1"
-                        data-emit-metadata="0"
-                        data-input-position="bottom"
-                        data-theme="preferred_color_scheme"
-                        data-lang="en"
-                        data-loading="lazy"
-                        crossorigin="anonymous"
-                        async>
-                </script>
-            </section>
+            <?php
+                require($_SERVER['DOCUMENT_ROOT']."/parts/comments.php");
+            ?>
         </main>
         <footer>
             <?php
