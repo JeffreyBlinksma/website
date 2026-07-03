87af1ee6e1024499e0a0d6f1b5dcd13320b0c79d
Jeffrey Blinksma <54038477+JeffreyBlinksma@users.noreply.github.com>
2026-07-03 20:13:47 +0100

    Change Leaflet tile URL to proxy

--- visited/index.php
+++ visited/index.php
@@ -19,7 +19,7 @@
             <div id="map"></div>
             <script>
                 let map = L.map('map').setView([51.505, -0.09], 13);
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                L.tileLayer('/visited/tiles/{z}/{x}/{y}.png', {
                     maxZoom: 19,
                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 }).addTo(map);
